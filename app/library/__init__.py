7f02f907cf783b1f7c16ecac285903048853c020
Mehmet Birgi <mbirgi@gmail.com>
2020-05-20 10:20:36 +0200

    refactored back end

--- app/library/__init__.py
+++ app/library/__init__.py
@@ -1,81 +1,15 @@
-# import os
-from .. import db
-from ..models import Track, Artist, Album, Playlist
-
 from .json_library import JSONLibrary
 from .sql_library import SQLLibrary
-
-# _dev = os.getenv('MMLIB_DEV_MODE')
 
 lib = SQLLibrary()
 
 
 class Library():
-    def save_tracks(self, tracks):
-        pass
+    def save_tracks(self, sp_tracks):
+        lib.save_tracks(sp_tracks)
 
     def update_saved_tracks(self, sp_tracks):
-        for sp_track in sp_tracks:
-            lib_track = Track.query.get(sp_track['id']) or self._make_db_track(sp_track)
-            lib_track.is_saved_track = True
-            db.session.add(lib_track)
-        db.session.commit()
+        lib.update_saved_tracks(sp_tracks)
 
     def update_saved_albums(self, sp_albums):
-        for sp_album in sp_albums:
-            lib_album = Track.query.get(sp_album['id']) or self._make_db_album(sp_album)
-            lib_album.is_saved_album = True
-            db.session.add(lib_album)
-        db.session.commit()
-
-    def _make_db_track(self, sp_track):
-        db_track = Track(
-            id=sp_track['id'],
-            name=sp_track['name'],
-            duration_ms=sp_track['duration_ms'],
-            track_number=sp_track['track_number'],
-            danceability=sp_track.get('danceability'),
-            energy=sp_track.get('energy'),
-            key=sp_track.get('key'),
-            loudness=sp_track.get('loudness'),
-            mode=sp_track.get('mode'),
-            speechiness=sp_track.get('speechiness'),
-            acousticness=sp_track.get('acousticness'),
-            instrumentalness=sp_track.get('instrumentalness'),
-            liveness=sp_track.get('liveness'),
-            valence=sp_track.get('valence'),
-            tempo=sp_track.get('tempo'),
-            time_signature=sp_track.get('time_signature'),
-        )
-        db.session.add(db_track)
-        self._add_item_artists(db_track, sp_track['artists'])
-        return db_track
-
-    def _add_item_artists(self, item, artists):
-        for artist in artists:
-            item_artist = Artist.query.get(artist['id']) or self._make_db_artist(artist)
-            item.artists.append(item_artist)
-
-    def _add_item_tracks(self, item, tracks):
-        for track in tracks:
-            item_track = Track.query.get(track['id']) or self._make_db_track(track)
-            item.tracks.append(item_track)
-
-    def _make_db_artist(self, artist):
-        db_artist = Artist(
-            id=artist['id'],
-            name=artist['name'],
-        )
-        db.session.add(db_artist)
-        return db_artist
-
-    def _make_db_album(self, sp_album):
-        db_album = Album(
-            id=sp_album['id'],
-            name=sp_album['name'],
-            total_tracks=sp_album['total_tracks']
-        )
-        db.session.add(db_album)
-        self._add_item_artists(db_album, sp_album['artists'])
-        self._add_item_tracks(db_album, sp_album['tracks'])
-        return db_album
+        lib.update_saved_albums(sp_albums)
--- app/library/sql_library.py
+++ app/library/sql_library.py
@@ -1,122 +1,60 @@
 from .. import db
 from ..models import Track, Artist, Album, Playlist
-from .. import spotify
 
 
 class SQLLibrary():
     def __init__(self):
         pass
 
-    def refresh_from_spotify(self, spotify_library):
-        self._update_saved_tracks(spotify_library['saved_tracks'])
-        self._update_saved_albums(spotify_library['saved_albums'])
-        self._update_saved_playlists(spotify_library['playlists'])
-        # self._update_audio_features()
-        # self._save_last_import_dt()
+    def save_tracks(self, sp_tracks):
+        for sp_track in sp_tracks:
+            lib_track = Track.query.get(sp_track['id'])
+            if lib_track is None:
+                lib_track = self._make_db_track(sp_track)
+            else:
+                self._update_lib_track(lib_track, sp_track)
+            db.session.add(lib_track)
         db.session.commit()
 
-    # def _update_audio_features(self):
-    #     features = spotify.get_audio_features_for_tracks([track.id for track in Track.query.all()])
-    #     for item in features:
-    #         track = Track.query.get(item['id'])
-    #         track.danceability = item.get('danceability')
-    #         track.energy = item.get('energy')
-    #         track.key = item.get('key')
-    #         track.loudness = item.get('loudness')
-    #         track.mode = item.get('mode')
-    #         track.speechiness = item.get('speechiness')
-    #         track.acousticness = item.get('acousticness')
-    #         track.instrumentalness = item.get('instrumentalness')
-    #         track.liveness = item.get('liveness')
-    #         track.valence = item.get('valence')
-    #         track.tempo = item.get('tempo')
-    #         track.duration_ms = item.get('duration_ms')
-    #         track.time_signature = item.get('time_signature')
-    #         db.session.add(track)
+    def update_saved_tracks(self, sp_tracks):
+        for sp_track in sp_tracks:
+            lib_track = Track.query.get(sp_track['id']) or self._make_db_track(sp_track)
+            lib_track.is_saved_track = True
+            db.session.add(lib_track)
+        db.session.commit()
 
+    def update_saved_albums(self, sp_albums):
+        for sp_album in sp_albums:
+            lib_album = Album.query.get(sp_album['id']) or self._make_db_album(sp_album)
+            lib_album.is_saved_album = True
+            db.session.add(lib_album)
+        db.session.commit()
 
+    def _update_lib_track(self, lib_track, sp_track):
+        lib_track.id = sp_track['id']
+        lib_track.name = sp_track['name']
+        lib_track.duration_ms = sp_track['duration_ms']
+        lib_track.track_number = sp_track['track_number']
+        lib_track.danceability = sp_track.get('danceability')
+        lib_track.energy = sp_track.get('energy')
+        lib_track.key = sp_track.get('key')
+        lib_track.loudness = sp_track.get('loudness')
+        lib_track.mode = sp_track.get('mode')
+        lib_track.speechiness = sp_track.get('speechiness')
+        lib_track.acousticness = sp_track.get('acousticness')
+        lib_track.instrumentalness = sp_track.get('instrumentalness')
+        lib_track.liveness = sp_track.get('liveness')
+        lib_track.valence = sp_track.get('valence')
+        lib_track.tempo = sp_track.get('tempo')
+        lib_track.time_signature = sp_track.get('time_signature')
+        return lib_track
 
-    def _update_saved_albums(self, saved_albums):
-        for album in saved_albums:
-            self._write_saved_album(album)
-
-    def _update_saved_playlists(self, saved_playlists):
-        for playlist in saved_playlists:
-            self._write_saved_playlist(playlist)
-
-    def _update_saved_tracks(self, tracks):
-        for track in tracks:
-            self._write_saved_track(track)
-
-    def _write_saved_album(self, saved_album):
-        album = Album.query.get(saved_album['id']) or self._make_db_album(saved_album)
-        if not album.is_saved_album:
-            album.is_saved_album = True
-            db.session.add(album)
-
-    def _write_saved_playlist(self, saved_playlist):
-        playlist = Playlist.query.get(saved_playlist['id']) or self._make_db_playlist(saved_playlist)
-        if not playlist.is_saved_playlist:
-            playlist.is_saved_playlist = True
-            db.session.add(playlist)
-
-    def _write_saved_track(self, saved_track):
-        track = Track.query.get(saved_track['id']) or self._make_db_track(saved_track)
-        if not track.is_saved_track:
-            track.is_saved_track = True
-            db.session.add(track)
-
-    def _make_db_album(self, album):
-        db_album = Album(
-            id=album['id'],
-            name=album['name'],
-            total_tracks=album['total_tracks']
-        )
-        db.session.add(db_album)
-        self._add_item_artists(db_album, album['artists'])
-        self._add_item_tracks(db_album, album['tracks'])
-        return db_album
-
-    def _make_db_artist(self, artist):
-        db_artist = Artist(
-            id=artist['id'],
-            name=artist['name'],
-        )
-        db.session.add(db_artist)
-        return db_artist
-
-    def _make_db_playlist(self, playlist):
-        db_playlist = Playlist(
-            id=playlist['id'],
-            name=playlist['name'],
-            description=playlist['description'],
-        )
-        db.session.add(db_playlist)
-        self._add_item_tracks(db_playlist, playlist['tracks'])
-        return db_playlist
-
-    def _make_db_track(self, track):
-        db_track = Track(
-            id=track['id'],
-            name=track['name'],
-            duration_ms=track['duration_ms'],
-            track_number=track['track_number'],
-            danceability=track.get('danceability'),
-            energy=track.get('energy'),
-            key=track.get('key'),
-            loudness=track.get('loudness'),
-            mode=track.get('mode'),
-            speechiness=track.get('speechiness'),
-            acousticness=track.get('acousticness'),
-            instrumentalness=track.get('instrumentalness'),
-            liveness=track.get('liveness'),
-            valence=track.get('valence'),
-            tempo=track.get('tempo'),
-            time_signature=track.get('time_signature'),
-        )
-        db.session.add(db_track)
-        self._add_item_artists(db_track, track['artists'])
-        return db_track
+    def _make_db_track(self, sp_track):
+        lib_track = Track()
+        self._update_lib_track(lib_track, sp_track)
+        db.session.add(lib_track)
+        self._add_item_artists(lib_track, sp_track['artists'])
+        return lib_track
 
     def _add_item_artists(self, item, artists):
         for artist in artists:
@@ -128,6 +66,46 @@ class SQLLibrary():
             item_track = Track.query.get(track['id']) or self._make_db_track(track)
             item.tracks.append(item_track)
 
+    def _make_db_artist(self, artist):
+        db_artist = Artist(
+            id=artist['id'],
+            name=artist['name'],
+        )
+        db.session.add(db_artist)
+        return db_artist
+
+    def _make_db_album(self, sp_album):
+        db_album = Album(
+            id=sp_album['id'],
+            name=sp_album['name'],
+            total_tracks=sp_album['total_tracks']
+        )
+        db.session.add(db_album)
+        self._add_item_artists(db_album, sp_album['artists'])
+        self._add_item_tracks(db_album, sp_album['tracks'])
+        return db_album
+
+    def _make_db_playlist(self, playlist):
+        db_playlist = Playlist(
+            id=playlist['id'],
+            name=playlist['name'],
+            description=playlist['description'],
+        )
+        db.session.add(db_playlist)
+        self._add_item_tracks(db_playlist, playlist['tracks'])
+        return db_playlist
+
+
+    # def _update_saved_playlists(self, saved_playlists):
+    #     for playlist in saved_playlists:
+    #         self._write_saved_playlist(playlist)
+    #
+    # def _write_saved_playlist(self, saved_playlist):
+    #     playlist = Playlist.query.get(saved_playlist['id']) or self._make_db_playlist(saved_playlist)
+    #     if not playlist.is_saved_playlist:
+    #         playlist.is_saved_playlist = True
+    #         db.session.add(playlist)
+    #
     # def _add_artists(self, artists):
     #     for artist in artists:
     #         if not Artist.query.get(artist['id']):
