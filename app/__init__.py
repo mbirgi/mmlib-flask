c006c46ca597f28c67d1f8b2f81917a5dab6ce63
Mehmet Birgi <mbirgi@gmail.com>
2020-05-24 19:15:52 +0200

    switched to kvsession

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_kvsession import KVSessionExtension
 from flask_migrate import Migrate
 # from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from simplekv.fs import FilesystemStore
 
 from config import config
+
+store = FilesystemStore('./data/session')
+kvsession = KVSessionExtension()
 
 bootstrap = Bootstrap()
 # mail = Mail()
@@ -19,6 +24,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    kvsession.init_app(app, session_kvstore=store)
     bootstrap.init_app(app)
     # mail.init_app(app)
     moment.init_app(app)
@@ -29,4 +35,3 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     return app
-
